ff6d0c3eacdbbea0ccb2b25c61f66316ad524854
jhyang22 <jaeho9229@gmail.com>
2025-05-27 10:49:06 +0900

    chore : git init

---
+++ src/test/java/com/study/petory/PetoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.petory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

7ae9bf46f08074d49980095652abb80d33adca04
deabaind <deabaind@gmail.com>
2025-05-30 22:55:16 +0900

    feat: env 설정을 위한 dotenv-java 설정 추가

--- src/main/java/com/study/petory/domain/user/entity/UserRole.java
+++ src/main/java/com/study/petory/domain/user/entity/UserRole.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,4 +23,8 @@ public class UserRole {
 
 	@Column(nullable = false)
 	private Role role;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
 }

377ff7a0cd69087f4127bff9da674444c5b850ee
jhyang22 <jaeho9229@gmail.com>
2025-05-31 00:37:33 +0900

    feat : controller 기초 설정

--- src/main/java/com/study/petory/domain/place/controller/PlaceController.java
+++ src/main/java/com/study/petory/domain/place/controller/PlaceController.java
@@ -1,7 +1,18 @@
 package com.study.petory.domain.place.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.study.petory.domain.place.service.PlaceService;
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/places")
 public class PlaceController {
+
+	private final PlaceService placeService;
+
+
 }

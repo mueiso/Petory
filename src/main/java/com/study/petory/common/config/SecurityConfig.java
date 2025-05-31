604f50f6d504cbe63cf2e6c5329ca88b413b346e
Jess <jcscene@gmail.com>
2025-05-31 22:01:15 +0900

    docs: 주석 정리

--- src/main/java/com/study/petory/common/config/SecurityConfig.java
+++ src/main/java/com/study/petory/common/config/SecurityConfig.java
@@ -21,32 +21,37 @@ public class SecurityConfig {
 
 	private final JwtFilter jwtFilter;
 
+	/*
+	1. .csrf : CSRF 설정 → JWT 기반이기 때문에 csrf 보호 비활성화
+	2. .sessionManagement : 세션 관리 방식 설정
+	3. .authorizeHttpRequests : URL 접근 권한 설정
+			.requestMatchers : 해당 경로들은 로그인 없이 접근 허용 → permitAll(누구나 접근 허용)
+			.anyRequest().authenticated() : 그 외의 모든 요청은 인증된 사용자만 접근 가능
+	4. exceptionHandling : 인증 예외 처리 커스터마이징 → 인증 안 된 사용자가 보호된 리소스 접근 시 예외를 JSON 으로 반환
+			.authenticationEntryPoint : 인증 안 된 사용자 접근 시 동작 지정
+			response.setStatue : HTTP 상태 코드 401 UNAUTHORIZED 로 응답
+			response.setContentType : 응답 형식을 JSON 으로 설정
+			response.getWriter().write() : 사용자에게 JSON 메시지를 반환
+	5. addFilterBefore : JWT 필터를 Security 필터 체인에 등록 → JWT 토큰 유효한지 먼저 검증 후 인증 처리 진행
+	 */
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		// CSRF 설정 → JWT 기반이기 때문에 csrf 보호 비활성화
 		http.csrf(AbstractHttpConfigurer::disable)
-			// 세션 관리 방식 설정
+			.formLogin(AbstractHttpConfigurer::disable)
+			.logout(AbstractHttpConfigurer::disable)
 			.sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-			// URL 접근 권한 설정
 			.authorizeHttpRequests(auth -> auth
-				// 해당 경로들은 로그인 없이 접근 허용 → permitAll(누구나 접근 허용)
-				.requestMatchers("/auth/**", "/users/signup", "/users/login", "/error", "/oauth2/**").permitAll()
-				// 그 외의 모든 요청은 인등된 사용자만 접근 가능
+				// TODO 수정 필요
+				.requestMatchers("/auth/**", "/users/signup", "/users/login", "/error", "/oauth2/").permitAll()
 				.anyRequest().authenticated()
 			)
-			// 인증 예외 처리 커스터마이징 → 인증 X 사용자사 보호된 리소스 접근 시 예외를 JSON 으로 반환
 			.exceptionHandling(ex -> ex
-				// 인증 안 된 사용자 접근 시 동작 지정
 				.authenticationEntryPoint((request, response, authException) -> {
-					// HTTP 산태코드 401 UNAUTHORIZED 로 응답
 					response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-					// 응답 형식을 JSON 으로 설정
 					response.setContentType("application/json;charset=UTF-8");
-					// 사용자에게 JSON 메시지를 반환
 					response.getWriter().write("{\"status\":401,\"message\":\"인증이 필요합니다.\"}");
 				})
 			)
-			// JWT 필터를 Security 필터 체인에 등록 → JWT 토큰 유효한지 먼저 검증 후 인증 처리 진행
 			.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
 		return http.build();

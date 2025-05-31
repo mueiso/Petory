577da1bfb2c156abebb7b2e90844f88541984cd5
jhyang22 <jaeho9229@gmail.com>
2025-05-31 20:51:38 +0900

    docs : 주석 추가

--- src/main/java/com/study/petory/domain/place/entity/Status.java
+++ src/main/java/com/study/petory/domain/place/entity/Status.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 public enum Status {
 	ACTIVE("영업중"),
 	INACTIVE("영업 중지"),
-	DELETED("폐업");
+	DELETED("폐업"); // -> 잘못 신고가 들어와서 폐업으로 오인할 수도 있어서 일단 소프트 딜리트 하고 일정 기간 이후에 하드 딜리트 되도록!
 
 	private final String displayName;
 

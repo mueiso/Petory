f07de2b9a0897a852e4cbc3a3540c1dfb5915303
Eunji Lee <aggee212@gmail.com>
2025-05-31 14:03:31 +0900

    feat: ownerboard 엔티티 및 dto생성
    request dto 생성

--- src/main/java/com/study/petory/domain/ownerBoard/dto/request/OwnerBoardCreateRequestDto.java
+++ src/main/java/com/study/petory/domain/ownerBoard/dto/request/OwnerBoardCreateRequestDto.java
@@ -2,16 +2,21 @@ package com.study.petory.domain.ownerBoard.dto.request;
 
 import java.util.List;
 
+import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import lombok.Singular;
 
 @Getter
 public class OwnerBoardCreateRequestDto {
 
-	@NotBlank
+	@NotBlank(message = "제목은 필수 입력값 입니다.")
+	@Size(max = 30, message = "최대 30자까지 입력할 수 있습니다.")
 	private String title;
 
-	@NotBlank
+	@NotBlank(message = "내용은 필수 입력값 입니다.")
+	@Column(columnDefinition = "TEXT")
 	private String content;
 
 	// 사진 여러개 받기 가능
--- src/main/java/com/study/petory/domain/ownerBoard/entity/OwnerBoard.java
+++ src/main/java/com/study/petory/domain/ownerBoard/entity/OwnerBoard.java
@@ -1,11 +1,14 @@
 package com.study.petory.domain.ownerBoard.entity;
 
 import com.study.petory.common.entity.BaseEntityWithBothAt;
+import com.study.petory.domain.user.entity.User;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class OwnerBoard extends BaseEntityWithBothAt {
@@ -17,5 +20,11 @@ public class OwnerBoard extends BaseEntityWithBothAt {
 	private String title;
 
 	private String content;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+
 
 }

377b480a7ad2e5d3cd0ffbc6c78fcbcd9214c98a
deabaind <deabaind@gmail.com>
2025-06-02 00:17:52 +0900

    feat: 입력을 받는 dto 생성

---
+++ src/main/java/com/study/petory/domain/dailyQna/dto/request/DailyQnaUpdateRequestDto.java
@@ -0,0 +1,10 @@
+package com.study.petory.domain.dailyQna.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class DailyQnaUpdateRequestDto {
+	private String answer;
+}

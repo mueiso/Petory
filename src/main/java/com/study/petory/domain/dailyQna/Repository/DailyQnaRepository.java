c2136d397eaa5883cf370568f5e1f6c20054b70f
deabaind <deabaind@gmail.com>
2025-06-01 21:53:45 +0900

    feat: 사용자 id, 질문 id로 답변 검색 메서드 생성

--- src/main/java/com/study/petory/domain/dailyQna/Repository/DailyQnaRepository.java
+++ src/main/java/com/study/petory/domain/dailyQna/Repository/DailyQnaRepository.java
@@ -1,7 +1,10 @@
 package com.study.petory.domain.dailyQna.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.study.petory.domain.dailyQna.dto.response.DailyQnaGetResponseDto;
 import com.study.petory.domain.dailyQna.entity.DailyQna;
 import com.study.petory.exception.CustomException;
 import com.study.petory.exception.enums.ErrorCode;
@@ -11,4 +14,6 @@ public interface DailyQnaRepository extends JpaRepository<DailyQna, Long> {
 	default DailyQna findByIdOrElseThrow(Long dailyQnaId) {
 		return findById(dailyQnaId).orElseThrow(() -> new CustomException(ErrorCode.DAILY_QNA_NOT_FOUND));
 	}
+
+	List<DailyQna> findByUserId_IdAndQuestionId_Id(Long userIdId, Long questionIdId);
 }

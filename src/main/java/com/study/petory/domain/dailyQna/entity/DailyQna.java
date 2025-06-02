e54ccffc562077eaf751305aace5a405e345971a
deabaind <deabaind@gmail.com>
2025-06-02 17:42:36 +0900

    chore: 참조 필드 Lazy 적용
    혼동할 수 있는 필드명 통일
    @SQLRestriction -> @Where 어노테이션 수정

--- src/main/java/com/study/petory/domain/dailyQna/entity/DailyQna.java
+++ src/main/java/com/study/petory/domain/dailyQna/entity/DailyQna.java
@@ -1,25 +1,27 @@
 package com.study.petory.domain.dailyQna.entity;
 
-import org.hibernate.annotations.SQLRestriction;
+import org.hibernate.annotations.Where;
 
 import com.study.petory.common.entity.BaseEntityWithBothAt;
 import com.study.petory.domain.user.entity.User;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @Table(name = "tb_daily_qna")
-@SQLRestriction("deleted_at is null")
+@Where(clause = "deleted_at is null")
 @NoArgsConstructor
 public class DailyQna extends BaseEntityWithBothAt {
 	@Id
@@ -27,19 +29,20 @@ public class DailyQna extends BaseEntityWithBothAt {
 	private Long id;
 
 	@JoinColumn(nullable = false)
-	@ManyToOne
-	private User userId;
+	@ManyToOne(fetch = FetchType.LAZY)
+	private User user;
 
 	@JoinColumn(nullable = false)
-	@ManyToOne
-	private Question questionId;
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Question question;
 
 	@Column(nullable = false, length = 255)
 	private String answer;
 
-	public DailyQna(User userId, Question questionId, String answer) {
-		this.userId = userId;
-		this.questionId = questionId;
+	@Builder
+	public DailyQna(User user, Question question, String answer) {
+		this.user = user;
+		this.question = question;
 		this.answer = answer;
 	}
 

3f364866a0309b79e301ba8bca04e3170b0a4a54
jhyang22 <jaeho9229@gmail.com>
2025-06-02 21:07:56 +0900

    feat : 평균 평점 계산 로직 추가

--- src/main/java/com/study/petory/domain/place/repository/PlaceReviewRepository.java
+++ src/main/java/com/study/petory/domain/place/repository/PlaceReviewRepository.java
@@ -1,5 +1,7 @@
 package com.study.petory.domain.place.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -17,4 +19,6 @@ public interface PlaceReviewRepository extends JpaRepository<PlaceReview, Long> 
 	}
 
 	Optional<PlaceReview> findByUserAndPlace(User user, Place place);
+
+	List<PlaceReview> findAllByPlaceAndDeletedAtIsNull(Place place);
 }
--- src/main/java/com/study/petory/domain/place/service/PlaceReviewServiceImpl.java
+++ src/main/java/com/study/petory/domain/place/service/PlaceReviewServiceImpl.java
@@ -1,5 +1,6 @@
 package com.study.petory.domain.place.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
@@ -22,7 +23,7 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class PlaceReviewServiceImpl implements PlaceReviewService{
+public class PlaceReviewServiceImpl implements PlaceReviewService {
 
 	private final PlaceReviewRepository placeReviewRepository;
 	private final PlaceRepository placeRepository;
@@ -39,7 +40,7 @@ public class PlaceReviewServiceImpl implements PlaceReviewService{
 		// 한 유저가 같은 장소에 한 개의 리뷰만 등록할 수 있도록 검증하는 로직
 		Optional<PlaceReview> findPlaceReview = placeReviewRepository.findByUserAndPlace(findUser, findPlace);
 
-		if(findPlaceReview.isPresent()) {
+		if (findPlaceReview.isPresent()) {
 			throw new CustomException(ErrorCode.DUPLICATE_REVIEW);
 		}
 
@@ -52,6 +53,9 @@ public class PlaceReviewServiceImpl implements PlaceReviewService{
 
 		placeReviewRepository.save(placeReview);
 
+		// 평균 평점 로직. private 메서드 활용
+		updatePlaceRatio(findPlace);
+
 		return PlaceReviewCreateResponseDto.from(placeReview);
 	}
 
@@ -62,11 +66,14 @@ public class PlaceReviewServiceImpl implements PlaceReviewService{
 		PlaceReviewUpdateRequestDto requestDto) {
 
 		// 해당 장소가 존재하는지 검증하기 위한 로직
-		placeRepository.findByIdOrElseThrow(placeId);
+		Place findPlace = placeRepository.findByIdOrElseThrow(placeId);
 
 		PlaceReview findPlaceReview = placeReviewRepository.findByIdOrElseThrow(reviewId);
 
 		findPlaceReview.updatePlaceReview(requestDto);
+
+		// 평균 평점 로직. private 메서드 활용
+		updatePlaceRatio(findPlace);
 
 		return PlaceReviewUpdateResponseDto.from(findPlaceReview);
 	}
@@ -77,16 +84,19 @@ public class PlaceReviewServiceImpl implements PlaceReviewService{
 	public void restorePlaceReview(Long placeId, Long reviewId) {
 
 		// 해당 장소가 존재하는지 검증하기 위한 로직
-		placeRepository.findByIdOrElseThrow(placeId);
+		Place findPlace = placeRepository.findByIdOrElseThrow(placeId);
 
 		PlaceReview findPlaceReview = placeReviewRepository.findByIdOrElseThrow(reviewId);
 
 		// deletedAt이 null 이라면 즉, 삭제되지 않았다면 복구가 안되므로 그것에 관한 검증 로직
-		if(findPlaceReview.getDeletedAt() == null) {
+		if (findPlaceReview.getDeletedAt() == null) {
 			throw new CustomException(ErrorCode.REVIEW_NOT_DELETED);
 		}
 
 		findPlaceReview.restoreEntity();
+
+		// 평균 평점 로직. private 메서드 활용
+		updatePlaceRatio(findPlace);
 	}
 
 	// 리뷰 삭제
@@ -95,21 +105,30 @@ public class PlaceReviewServiceImpl implements PlaceReviewService{
 	public void deletePlaceReview(Long placeId, Long reviewId) {
 
 		// 해당 장소가 존재하는지 검증하기 위한 로직
-		placeRepository.findByIdOrElseThrow(placeId);
+		Place findPlace = placeRepository.findByIdOrElseThrow(placeId);
 
 		PlaceReview findPlaceReview = placeReviewRepository.findByIdOrElseThrow(reviewId);
 
 		// deletedAt이 null 이 아니라면 즉, 삭제되었다면 삭제가 안되므로 그것에 관한 검증 로직
-		if(findPlaceReview.getDeletedAt() != null) {
+		if (findPlaceReview.getDeletedAt() != null) {
 			throw new CustomException(ErrorCode.ALREADY_DELETED_REVIEW);
 		}
 
 		findPlaceReview.deactivateEntity();
+
+		// 평균 평점 로직. private 메서드 활용
+		updatePlaceRatio(findPlace);
 	}
 
 	// 다른 서비스에서 사용가능하게 설정한 메서드
 	@Override
 	public PlaceReview findPlaceReviewByReviewIdOrElseThrow(Long placeReviewId) {
 		return placeReviewRepository.findByIdOrElseThrow(placeReviewId);
+	}
+
+	// 평점 업데이트 로직의 중복 사용으로 인한 분리 로직
+	private void updatePlaceRatio(Place place) {
+		List<PlaceReview> findPlaceReviewList = placeReviewRepository.findAllByPlaceAndDeletedAtIsNull(place);
+		place.updateRatio(findPlaceReviewList);
 	}
 }

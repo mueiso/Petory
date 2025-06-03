400887971801238f65946639f725a6e31355366e
Eunji Lee <aggee212@gmail.com>
2025-06-03 22:33:03 +0900

    style: requestDto -> CreateRequestDto로 이름 변경

--- src/main/java/com/study/petory/domain/ownerBoardComment/dto/request/OwnerBoardCommentCreateRequestDto.java
+++ src/main/java/com/study/petory/domain/ownerBoardComment/dto/request/OwnerBoardCommentCreateRequestDto.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
-public class OwnerBoardCommentRequestDto {
+public class OwnerBoardCommentCreateRequestDto {
 
 	@NotBlank(message = "내용은 필수 입력 값입니다.")
 	@Size(max = 80, message = "최대 80자까지 입력할 수 있습니다.")

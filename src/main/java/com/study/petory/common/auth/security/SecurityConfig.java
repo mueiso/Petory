1f5c9f3925478e129eb59c602d76a575b7c6276b
Jess <jcscene@gmail.com>
2025-06-04 23:01:14 +0900

    docs: 중복 클래스 제거

--- src/main/java/com/study/petory/common/auth/security/SecurityConfig.java
+++ src/main/java/com/study/petory/common/auth/security/SecurityConfig.java
@@ -12,6 +12,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.study.petory.common.auth.service.CustomOAuth2UserService;
+
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 

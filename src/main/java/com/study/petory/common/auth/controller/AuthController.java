3027436bd24e75b873da4eece3184f63b7f8678e
Jess <jcscene@gmail.com>
2025-06-05 21:08:24 +0900

    feat: SuccessCode_User 추가

--- src/main/java/com/study/petory/common/auth/controller/AuthController.java
+++ src/main/java/com/study/petory/common/auth/controller/AuthController.java
@@ -8,10 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.nimbusds.oauth2.sdk.TokenResponse;
 import com.study.petory.common.auth.dto.OAuth2LoginRequestDto;
+import com.study.petory.common.auth.dto.TokenReissueRequestDto;
+import com.study.petory.common.auth.dto.TokenResponseDto;
 import com.study.petory.common.auth.service.AuthService;
 import com.study.petory.common.response.CommonResponse;
+import com.study.petory.exception.enums.SuccessCode;
 
 import lombok.RequiredArgsConstructor;
 
@@ -24,9 +26,9 @@ public class AuthController {
 
 	// OAuth2 로그인 후 토큰 발급
 	@PostMapping("/login/oauth2")
-	public CommonResponse<TokenResponse> oauth2Login(@RequestBody OAuth2LoginRequestDto loginRequestDto) {
-		TokenResponse tokenResponse = authService.issueToken(loginRequestDto.toUser());
-		return CommonResponse.of(SuccessCode.SUCCESS_USER_LOGIN, tokenResponse);
+	public CommonResponse<TokenResponseDto> oauth2Login(@RequestBody OAuth2LoginRequestDto loginRequestDto) {
+		TokenResponseDto tokenResponseDto = authService.issueToken(loginRequestDto.toUser());
+		return CommonResponse.of(SuccessCode.USER_LOGIN, tokenResponseDto);
 	}
 
 	// 로그아웃 처리
@@ -34,14 +36,14 @@ public class AuthController {
 	public CommonResponse<Object> logout(@RequestHeader("Authorization") String bearerToken,
 		@RequestParam String email) {
 		authService.logout(bearerToken, email);
-		return CommonResponse.of(SuccessCode.SUCCESS_USER_LOGOUT);
+		return CommonResponse.of(SuccessCode.USER_LOGOUT);
 	}
 
 	// Refresh Token 으로 Access Token 재발급
 	@PostMapping("/reissue")
-	public CommonResponse<TokenResponse> reissue(@RequestBody TokenReissueRequest request) {
-		TokenResponse tokenResponse = authService.reissue(request.getEmail(), request.getRefreshToken());
-		return CommonResponse.of(SuccessCode.SUCCESS_TOKEN_REISSUE, tokenResponse);
+	public CommonResponse<TokenResponseDto> reissue(@RequestBody TokenReissueRequestDto tokenReissueRequestDto) {
+		TokenResponseDto tokenResponseDto = authService.reissue(tokenReissueRequestDto.getEmail(),
+			tokenReissueRequestDto.getRefreshToken());
+		return CommonResponse.of(SuccessCode.TOKEN_REISSUE, tokenResponseDto);
 	}
 }
-
--- src/main/java/com/study/petory/exception/enums/SuccessCode.java
+++ src/main/java/com/study/petory/exception/enums/SuccessCode.java
@@ -4,11 +4,16 @@ import org.springframework.http.HttpStatus;
 
 public enum SuccessCode implements BaseCode {
 
-	// Common
+	// COMMON
 	OK(HttpStatus.OK, "요청이 성공적으로 처리되었습니다."),
 	CREATED(HttpStatus.CREATED, "요청이 성공적으로 생성되었습니다."),
-	NO_CONTENT(HttpStatus.NO_CONTENT, "요청이 성공적으로 처리되었습니다.");
+	DELETED(HttpStatus.OK, "요청이 성공적으로 처리되었습니다."),
 
+	// USER
+	USER_LOGIN(HttpStatus.OK, "성공적으로 로그인되었습니다."),
+	USER_LOGOUT(HttpStatus.OK, "성공적으로 로그아웃되었습니다."),
+	ADMIN_LOGIN(HttpStatus.OK, "성공적으로 관리자로 로그인되었습니다."),
+	TOKEN_REISSUE(HttpStatus.OK, "성공적으로 토큰이 재발급되었습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String message;

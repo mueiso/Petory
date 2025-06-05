03d634afae122db18cdaea1c78edfa70d09c3459
chaewon9999 <w0729@naver.com>
2025-06-05 15:17:06 +0900

    feat: ChatRoom Document 구현

---
+++ src/main/java/com/study/petory/domain/chat/entity/ChatRoom.java
@@ -0,0 +1,30 @@
+package com.study.petory.domain.chat.entity;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.study.petory.common.entity.BaseEntityWithCreatedAt;
+
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Document(collection = "chatroom")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatRoom extends BaseEntityWithCreatedAt {
+
+	@Id
+	private String id;
+
+	private Long sellerId; // 판매자 닉네임
+
+	private Long customerId; // 구매자 닉네임
+
+	private Long boardId; //판매글 Id
+
+	private String lastMessageId;
+}

a6b73173e4b11524fa97654311d0dcba633ce26c
deabaind <deabaind@gmail.com>
2025-06-05 12:47:37 +0900

    feat: 활성화가 아닌 질문을 조회할 때의 에러 코드 생성

--- src/main/java/com/study/petory/exception/enums/ErrorCode.java
+++ src/main/java/com/study/petory/exception/enums/ErrorCode.java
@@ -50,8 +50,8 @@ public enum ErrorCode implements BaseCode {
 	// Question
 	QUESTION_NOT_FOUND(HttpStatus.NOT_FOUND, "질문이 존재하지 않습니다."),
 	DATE_IS_EXIST(HttpStatus.CONFLICT, "해당 날짜에는 질문이 이미 존재합니다."),
-	QUESTION_IS_DEACTIVATED(HttpStatus.BAD_REQUEST, "이미 비활성화된 질문입니다.")
-
+	QUESTION_IS_DEACTIVATED(HttpStatus.BAD_REQUEST, "이미 비활성화된 질문입니다."),
+	QUESTION_IS_NOT_DEACTIVATED(HttpStatus.BAD_REQUEST, "비활성화된 질문이 아닙니다.")
 	;
 
 	private final HttpStatus httpStatus;

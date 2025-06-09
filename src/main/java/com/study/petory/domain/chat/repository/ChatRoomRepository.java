0e046bbcd0860915502fd3f102d9d291375200a7
chaewon9999 <w0729@naver.com>
2025-06-09 23:15:42 +0900

    feat: repository 에 쿼리 추가 후 테스트 완료
    쿼리dsl로 리팩토링 예정

--- src/main/java/com/study/petory/domain/chat/repository/ChatRoomRepository.java
+++ src/main/java/com/study/petory/domain/chat/repository/ChatRoomRepository.java
@@ -1,10 +1,9 @@
 package com.study.petory.domain.chat.repository;
 
-import java.util.List;
-
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.study.petory.domain.chat.entity.ChatRoom;
 
@@ -12,5 +11,6 @@ public interface ChatRoomRepository extends MongoRepository<ChatRoom, String> {
 
 	ChatRoom findByTradeBoardIdAndSellerId(Long tradeBoardId, Long sellerId);
 
+	@Query("{$or: [{'sellerId': ?0}, {'customerId': ?0}], 'isDeleted': false}")
 	Slice<ChatRoom> findAllByUserIdAndIsDeletedFalse(Long customerId, PageRequest pageable);
 }

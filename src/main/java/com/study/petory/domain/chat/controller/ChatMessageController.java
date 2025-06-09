162cdf4844f1ac527ac7ce4c3bc887b34a0cb560
chaewon9999 <w0729@naver.com>
2025-06-09 11:35:46 +0900

    feat: message request Dto valid 예외처리

--- src/main/java/com/study/petory/domain/chat/controller/ChatMessageController.java
+++ src/main/java/com/study/petory/domain/chat/controller/ChatMessageController.java
@@ -1,14 +1,20 @@
 package com.study.petory.domain.chat.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import com.study.petory.common.response.CommonResponse;
 import com.study.petory.domain.chat.dto.request.ChatMessageSendRequestDto;
 import com.study.petory.domain.chat.entity.ChatMessage;
 import com.study.petory.domain.chat.service.ChatMessageService;
+import com.study.petory.exception.enums.ErrorCode;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -24,10 +30,15 @@ public class ChatMessageController {
 	 */
 	@MessageMapping("/message")
 	public void sendMessage(
-		@RequestBody ChatMessageSendRequestDto requestDto
+		@Valid ChatMessageSendRequestDto requestDto
 	) {
 		ChatMessage chatMessage = chatMessageService.createChatMessage(requestDto);
 
 		messagingTemplate.convertAndSend("/sub/room/" + requestDto.getChatRoomId(), chatMessage);
+	}
+
+	@MessageExceptionHandler(MethodArgumentNotValidException.class)
+	public void messageValidException(MethodArgumentNotValidException e) {
+		messagingTemplate.convertAndSend("/sub/errors", "잘못된 입력값이 존재합니다: " + e.getMessage());
 	}
 }
--- src/main/java/com/study/petory/domain/chat/dto/request/ChatMessageSendRequestDto.java
+++ src/main/java/com/study/petory/domain/chat/dto/request/ChatMessageSendRequestDto.java
@@ -1,5 +1,7 @@
 package com.study.petory.domain.chat.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -7,9 +9,12 @@ import lombok.Getter;
 @AllArgsConstructor
 public class ChatMessageSendRequestDto {
 
+	@NotBlank(message = "원하는 채팅방을 입력해주세요.")
 	private final String chatRoomId;
 
+	@NotNull(message = "전달하려는 사용자를 입력해주세요.")
 	private final Long senderId;
 
+	@NotBlank(message = "전달하려는 메시지를 입력해주세요.")
 	private final String message;
 }

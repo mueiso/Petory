a4174e216d9edf9129395d007c408cc3e4af07b8
Jess <jcscene@gmail.com>
2025-06-12 04:25:55 +0900

    test: OAuth2SuccessHandlerTest 구현중

--- src/main/java/com/study/petory/common/security/JwtFilter.java
+++ src/main/java/com/study/petory/common/security/JwtFilter.java
@@ -18,7 +18,6 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 // TODO - ResponseStatusException 대신 CustomException 으로 통일

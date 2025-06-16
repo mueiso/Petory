64510eb6ccf2cdf885d42c8d4af9abd4639c666a
Jess <jcscene@gmail.com>
2025-06-16 21:43:31 +0900

    feat: UserCleanupScheduler 구현 시작

--- src/main/java/com/study/petory/domain/user/repository/UserRepository.java
+++ src/main/java/com/study/petory/domain/user/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.study.petory.domain.user.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.EntityGraph;
@@ -15,4 +17,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	// userRole, userPrivateInfo 모두 필요 없는 경우
 	@EntityGraph(attributePaths = {"userRole"})
 	Optional<User> findUserById(Long id);
+
+	// 삭제일 85일~90일 사이인 사용자 (삭제 예정자)
+	List<User> findByDeletedAtBetween(LocalDateTime from, LocalDateTime to);
+
+	// 삭제일 90일 초과된 사용자 (하드 삭제 대상)
+	List<User> findByDeletedAtBefore(LocalDateTime cutoff);
 }

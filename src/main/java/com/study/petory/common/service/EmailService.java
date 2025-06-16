e61fd263d40ef30ef2db2b3b29aca924cfe47f3c
Jess <jcscene@gmail.com>
2025-06-17 00:18:49 +0900

    fix: UserCleanupScheduler 수정

--- src/main/java/com/study/petory/common/service/EmailService.java
+++ src/main/java/com/study/petory/common/service/EmailService.java
@@ -1,16 +1,15 @@
-// package com.study.petory.common.service;
-//
-// import java.time.LocalDateTime;
-//
-// import org.springframework.stereotype.Service;
-//
-// @Service
-// public class EmailService {
-//
-// 	public void sendDeletionWarning(String to, String name, LocalDateTime deletedAt) {
-//
-// 		// TODO: 실제 Email 전송 구현 (JavaMailSender + SMTP)
-// 		// log.info("이메일 전송: {}님께 계정 삭제 예정 안내 메일 발송 - 삭제일: {}", name, deletedAt.plusDays(90));
-// 	}
-// }
-//
+package com.study.petory.common.service;
+
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+	public void sendDeletionWarning(String to, String name, LocalDateTime deletedAt) {
+
+		// TODO: 실제 Email 전송 구현 (JavaMailSender + SMTP)
+		// log.info("이메일 전송: {}님께 계정 삭제 예정 안내 메일 발송 - 삭제일: {}", name, deletedAt.plusDays(90));
+	}
+}

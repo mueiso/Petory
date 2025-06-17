a7a9f05cd9741e803cafa6a1d39aa31e119215a1
Jess <jcscene@gmail.com>
2025-06-17 15:27:41 +0900

    feat: 인증 코드 전송 필요 Dto 구현

---
+++ src/main/java/com/study/petory/domain/user/dto/EmailVerificationConfirmRequestDto.java
@@ -0,0 +1,20 @@
+package com.study.petory.domain.user.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class EmailVerificationConfirmRequestDto {
+
+	@NotBlank
+	@Email
+	private String email;
+
+	@NotBlank
+	private String code;
+}

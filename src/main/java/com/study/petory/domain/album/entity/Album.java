7c850bca633444513a6f4b61b3a09bf5ae917f49
deabaind <deabaind@gmail.com>
2025-06-18 17:34:59 +0900

    refactor: user Lazy 적용

--- src/main/java/com/study/petory/domain/album/entity/Album.java
+++ src/main/java/com/study/petory/domain/album/entity/Album.java
@@ -13,6 +13,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -35,7 +36,7 @@ public class Album extends UpdateBasedEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "user_id", nullable = false)
 	private User user;
 

61f028ea129c999ef66caff728424bd4dbad157a
jhyang22 <jaeho9229@gmail.com>
2025-06-19 19:27:02 +0900

    style : 오탈자 수정

--- src/main/java/com/study/petory/domain/place/dto/request/PlaceReportRequestDto.java
+++ src/main/java/com/study/petory/domain/place/dto/request/PlaceReportRequestDto.java
@@ -5,7 +5,7 @@ import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-public class ReportPlaceRequestDto {
+public class PlaceReportRequestDto {
 
 	private final String content;
 }
--- src/main/java/com/study/petory/domain/place/repository/PlaceReportRepository.java
+++ src/main/java/com/study/petory/domain/place/repository/PlaceReportRepository.java
@@ -3,11 +3,11 @@ package com.study.petory.domain.place.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.study.petory.domain.place.entity.Place;
-import com.study.petory.domain.place.entity.ReportPlace;
+import com.study.petory.domain.place.entity.PlaceReport;
 import com.study.petory.domain.user.entity.User;
 
-public interface ReportPlaceRepository extends JpaRepository<ReportPlace, Long> {
+public interface PlaceReportRepository extends JpaRepository<PlaceReport, Long> {
 	boolean existsByUserAndPlace(User user, Place place);
 
-	long countReportPlaceByPlace(Place place);
+	long countPlaceReportByPlaceAndValid(Place place, boolean valid);
 }

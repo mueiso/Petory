43cdf79b66c89dfb700d179ca2f42e2c35248644
jhyang22 <jaeho9229@gmail.com>
2025-06-19 22:34:46 +0900

    refactor : 매개변수 리팩토링

--- src/main/java/com/study/petory/domain/place/repository/PlaceReportRepository.java
+++ src/main/java/com/study/petory/domain/place/repository/PlaceReportRepository.java
@@ -2,11 +2,9 @@ package com.study.petory.domain.place.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.study.petory.domain.place.entity.Place;
 import com.study.petory.domain.place.entity.PlaceReport;
-import com.study.petory.domain.user.entity.User;
 
 public interface PlaceReportRepository extends JpaRepository<PlaceReport, Long>, PlaceReportCustomRepository {
 
-	boolean existsByUserAndPlace(User user, Place place);
+	boolean existsByUserIdAndPlaceId(Long userId, Long placeId);
 }

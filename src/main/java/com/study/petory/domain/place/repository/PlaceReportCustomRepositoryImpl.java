f427078c466b8a3b83afbe7f04780cfa682eba3a
jhyang22 <jaeho9229@gmail.com>
2025-06-19 22:34:55 +0900

    style : 줄 맞춤

--- src/main/java/com/study/petory/domain/place/repository/PlaceReportCustomRepositoryImpl.java
+++ src/main/java/com/study/petory/domain/place/repository/PlaceReportCustomRepositoryImpl.java
@@ -10,7 +10,7 @@ import com.study.petory.domain.place.entity.QPlaceReport;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public class PlaceReportCustomRepositoryImpl implements PlaceReportCustomRepository{
+public class PlaceReportCustomRepositoryImpl implements PlaceReportCustomRepository {
 
 	private final JPAQueryFactory jpaQueryFactory;
 
@@ -23,15 +23,15 @@ public class PlaceReportCustomRepositoryImpl implements PlaceReportCustomReposit
 		booleanBuilder.and(qPlaceReport.place.id.eq(place.getId()));
 		booleanBuilder.and(qPlaceReport.isValid.eq(true));
 
-		if(place.getReportResetAt() != null) {
+		if (place.getReportResetAt() != null) {
 			booleanBuilder.and(qPlaceReport.createdAt.after(place.getReportResetAt()));
 		}
 
 		return Optional.ofNullable(jpaQueryFactory
-			.select(qPlaceReport.count())
-			.from(qPlaceReport)
-			.where(booleanBuilder)
-			.fetchOne())
+				.select(qPlaceReport.count())
+				.from(qPlaceReport)
+				.where(booleanBuilder)
+				.fetchOne())
 			.orElse(0L);
 	}
 }

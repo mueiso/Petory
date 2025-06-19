f16c040c99b1fc25ff72d6b7a97035720aa23806
Jess <jcscene@gmail.com>
2025-06-19 11:17:32 +0900

    feat: UserStatus 구현

---
+++ src/main/java/com/study/petory/domain/user/entity/UserStatus.java
@@ -0,0 +1,8 @@
+package com.study.petory.domain.user.entity;
+
+public enum UserStatus {
+	ACTIVE,       // 정상 상태
+	DEACTIVATED,  // 휴면 상태
+	SUSPENDED,    // 관리자에 의해 비활성화된 상태
+	DELETED       // hard delete 되기 전 soft delete 된 상태
+}

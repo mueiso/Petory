4cb60a86d3314cc82ffe59528734fcb1d6a931c8
jhyang22 <jaeho9229@gmail.com>
2025-06-19 22:29:06 +0900

    refactor : BooleanBuilder 사용으로 리팩토링

--- src/main/java/com/study/petory/domain/place/repository/PlaceReportCustomRepositoryImpl.java
+++ src/main/java/com/study/petory/domain/place/repository/PlaceReportCustomRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.study.petory.domain.place.repository;
 
 import java.util.Optional;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.study.petory.domain.place.entity.Place;
 import com.study.petory.domain.place.entity.QPlaceReport;
@@ -17,14 +18,19 @@ public class PlaceReportCustomRepositoryImpl implements PlaceReportCustomReposit
 
 	@Override
 	public long countPlaceReportByPlaceAndIsValidAndReportResetAt(Place place) {
+
+		BooleanBuilder booleanBuilder = new BooleanBuilder();
+		booleanBuilder.and(qPlaceReport.place.id.eq(place.getId()));
+		booleanBuilder.and(qPlaceReport.isValid.eq(true));
+
+		if(place.getReportResetAt() != null) {
+			booleanBuilder.and(qPlaceReport.createdAt.after(place.getReportResetAt()));
+		}
+
 		return Optional.ofNullable(jpaQueryFactory
 			.select(qPlaceReport.count())
 			.from(qPlaceReport)
-			.where(
-				qPlaceReport.place.id.eq(place.getId()),
-				qPlaceReport.isValid.eq(true),
-				place.getReportResetAt() == null ? null : qPlaceReport.createdAt.after(place.getReportResetAt())
-			)
+			.where(booleanBuilder)
 			.fetchOne())
 			.orElse(0L);
 	}

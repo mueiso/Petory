aa19720269c5ecac8915c8dd04c33011e8557bc7
chaewon9999 <w0729@naver.com>
2025-06-22 20:58:23 +0900

    docs: 주석 추가

--- src/main/java/com/study/petory/domain/chat/service/PresignedUrlService.java
+++ src/main/java/com/study/petory/domain/chat/service/PresignedUrlService.java
@@ -30,23 +30,29 @@ public class PresignedUrlService {
 
 	public PresignedUrlResponseDto createPresignedUrl(PresignedUrlRequestDto requestDto) {
 
+		// 파일 확장자 추출
 		s3Uploader.getExtension(requestDto.getFilename());
 
+		// 업로드될 S3 객체의 Key 경로 생성
 		String objectKey = "chat/" + requestDto.getChatRoomId() + "/" + requestDto.getFilename();
 
+		// 업로드 객체 생성
 		PutObjectRequest objectRequest = PutObjectRequest.builder()
 			.bucket(bucket)
 			.key(objectKey)
 			.contentType(requestDto.getContentType())
 			.build();
 
+		// Presigned Url 생성을 위한 요청 객체 생성
 		PutObjectPresignRequest presignRequest = PutObjectPresignRequest.builder()
-			.signatureDuration(Duration.ofMinutes(5))
+			.signatureDuration(Duration.ofMinutes(5)) //시간은 우선 5분으로 지정
 			.putObjectRequest(objectRequest)
 			.build();
 
+		// Presigned URL 생성
 		URL uploadUrl = s3Presigner.presignPutObject(presignRequest).url();
 
+		// 업로드 완료 후 접근 가능한 정적 파일 URL 생성
 		String fileUrl = "https://" + bucket + ".s3." + region + ".amazonaws.com/" + objectKey;
 
 		return new PresignedUrlResponseDto(uploadUrl.toString(), fileUrl);

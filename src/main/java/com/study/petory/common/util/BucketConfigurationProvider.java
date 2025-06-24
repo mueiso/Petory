1655e8203d6ba5b6cbb638e8ff70697a956f0f3d
jhyang22 <jaeho9229@gmail.com>
2025-06-24 20:23:13 +0900

    refactor : Bucket4j refill 전략 리팩토링

--- src/main/java/com/study/petory/common/util/BucketConfigurationProvider.java
+++ src/main/java/com/study/petory/common/util/BucketConfigurationProvider.java
@@ -14,11 +14,11 @@ public class BucketConfigurationProvider {
 	@Bean
 	public Supplier<BucketConfiguration> createBucketConfig() {
 
-		// 버킷 설정 Greedy 방식
+		// 버킷 설정 Intervally 방식
 		// 1시간에 최대 10개의 토큰 사용가능
 		// 토큰은 1시간 주기로 10개의 토큰을 한번에 채워줌
 		return () -> BucketConfiguration.builder()
-			.addLimit(limit -> limit.capacity(10).refillGreedy(10, Duration.ofHours(1)))
+			.addLimit(limit -> limit.capacity(10).refillIntervally(10, Duration.ofHours(1)))
 			.build();
 	}
 

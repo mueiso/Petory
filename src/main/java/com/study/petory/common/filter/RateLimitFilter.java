90a9eb7e1564496579b59f6927643927a6afb41f
jhyang22 <jaeho9229@gmail.com>
2025-06-24 10:31:22 +0900

    refactor : CommonResponse 형태로 응답 리팩토링

--- src/main/java/com/study/petory/common/filter/RateLimitFilter.java
+++ src/main/java/com/study/petory/common/filter/RateLimitFilter.java
@@ -11,6 +11,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.study.petory.common.exception.enums.ErrorCode;
+import com.study.petory.common.response.CommonResponse;
 import com.study.petory.common.security.CustomPrincipal;
 
 import io.github.bucket4j.Bucket;
@@ -30,6 +33,7 @@ public class RateLimitFilter extends OncePerRequestFilter {
 
 	private final ProxyManager<String> proxyManager;
 	private final Supplier<BucketConfiguration> bucketConfigurationSupplier = createBucketConfig();
+	private final ObjectMapper objectMapper;
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
@@ -67,18 +71,12 @@ public class RateLimitFilter extends OncePerRequestFilter {
 		if (bucket.tryConsume(1)) {
 			filterChain.doFilter(httpServletRequest, httpServletResponse);
 		} else {
-			httpServletResponse.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			CommonResponse<Object> errorResponse = new CommonResponse<>(ErrorCode.TOO_MANY_REQUESTS, null);
+
+			httpServletResponse.setStatus(HttpStatus.TOO_MANY_REQUESTS.value()); // ResponseEntity로 감싸지 않았기 때문에 필요
 			httpServletResponse.setCharacterEncoding("UTF-8"); // 한글 깨짐 방지
 			httpServletResponse.setContentType("application/json; charset=UTF-8");
-
-			String json = """
-				{
-				"status": 429,
-				"message": "요청이 너무 많습니다. 같은 요청은 1시간에 최대 10회까지 가능합니다."
-				}
-				""";
-
-			httpServletResponse.getWriter().write(json);
+			httpServletResponse.getWriter().write(objectMapper.writeValueAsString(errorResponse));
 		}
 	}
 

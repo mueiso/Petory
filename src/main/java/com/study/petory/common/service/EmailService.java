e2cc0ef9b6df9901aaa8733e96b1710bea5aec5a
Jess <jcscene@gmail.com>
2025-06-27 16:56:02 +0900

    refactor: 이메일 발송 로직 중복코드 제거 위한 메서드 구현

--- src/main/java/com/study/petory/common/service/EmailService.java
+++ src/main/java/com/study/petory/common/service/EmailService.java
@@ -25,7 +25,6 @@ public class EmailService {
 	private final JavaMailSender mailSender;  // 이메일 전송할 때 사용하는 JavaMailSender 객체
 	private final TemplateEngine templateEngine;  // HTML 템플릿을 렌더링하기 위한 Thymeleaf 템플릿 엔진
 
-
 	// soft delete 되어있는 계정에게 삭제 경고 이메일을 발송하는 메서드
 	public void sendDeletionWarning(String to, String name, LocalDateTime deletedAt) {
 
@@ -45,32 +44,7 @@ public class EmailService {
 		context.setVariable("deactivatedDate", deactivatedDate);
 		context.setVariable("deletionDate", deletionDate);
 
-		// "email/deletion-warning.html" 템플릿 파일 내 ${...} 표현식에 해당 변수를 삽입하여 완성된 HTML 을 생성
-		String htmlContent = templateEngine.process("email/deletion-warning", context);
-
-		try {
-			// 이메일 메시지 객체 생성
-			MimeMessage message = mailSender.createMimeMessage();
-			// 메시지를 쉽게 설정하기 위한 도우미 클래스 (다국어 인코딩 설정 포함)
-			MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
-
-			/*
-			 * 수신자 이메일 주소 설정
-			 * 발신자 이메일 주소 설정
-			 * 이메일 제목 설정
-			 * 이메일 본문을 HTML 형식으로 설정 (true: HTML 전송)
-			 */
-			helper.setTo(to);
-			helper.setFrom(FROM_EMAIL);
-			helper.setSubject("[Petory] 계정 삭제 예정 안내");
-			helper.setText(htmlContent, true);
-
-			// 이메일 전송
-			mailSender.send(message);
-			// TODO - 이메일 전송 중 예외 발생 시, 복구(재전송) 루틴 필요
-		} catch (MessagingException e) {
-			throw new RuntimeException("삭제 안내 이메일 전송 실패", e);
-		}
+		sendEmail(to, "[Petory] 계정 삭제 예정 안내", "email/deletion0warning", context);
 	}
 
 	// 휴면 계정으로 전환될 예정인 유저에게 안내 이메일 발송하는 메서드
@@ -83,22 +57,38 @@ public class EmailService {
 		context.setVariable("name", name);
 		context.setVariable("deactivationDate", deactivationDate);
 
-		// "email/deactivation-warning.html" 템플릿 파일 내 ${...} 표현식에 해당 변수를 삽입하여 완성된 HTML 을 생성
-		String htmlContent = templateEngine.process("email/deactivation-warning", context);
+		sendEmail(to, "[Petory] 장기 미접속 안내 - 곧 휴면 계정으로 전환됩니다", "email/deactivation-warning", context);
+	}
+
+	// 중복 로직 줄이기 위한 공통 이메일 발송 메서드
+	private void sendEmail(String to, String subject, String templatePath, Context context) {
 
 		try {
+			// 이메일 메시지 객체 생성
 			MimeMessage message = mailSender.createMimeMessage();
+			// 메시지를 쉽게 설정하기 위한 도우미 클래스 (다국어 인코딩 설정 포함)
 			MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
 
+			// "email/deletion-warning.html" 템플릿 파일 내 ${...} 표현식에 해당 변수를 삽입하여 완성된 HTML 을 생성
+			String htmlContent = templateEngine.process(templatePath, context);
+
+			/*
+			 * 수신자 이메일 주소 설정
+			 * 발신자 이메일 주소 설정
+			 * 이메일 제목 설정
+			 * 이메일 본문을 HTML 형식으로 설정 (true: HTML 전송)
+			 */
 			helper.setTo(to);
 			helper.setFrom(FROM_EMAIL);
-			helper.setSubject("[Petory] 장기 미접속 안내 - 곧 휴면 계정으로 전환됩니다");
+			helper.setSubject(subject);
 			helper.setText(htmlContent, true);
 
+			// 이메일 전송
 			mailSender.send(message);
 
+			// TODO - 이메일 전송 중 예외 발생 시, 복구(재전송) 루틴 필요
 		} catch (MessagingException e) {
-			throw new RuntimeException("휴면 안내 이메일 전송 실패", e);
+			throw new RuntimeException("이메일 전송 실패 - " + subject, e);
 		}
 	}
 }

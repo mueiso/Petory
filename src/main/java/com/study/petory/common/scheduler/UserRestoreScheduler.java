0b9f242a67fd44cfb5dd199d5cce73f927a6f9d6
Jess <jcscene@gmail.com>
2025-06-27 20:49:42 +0900

    fix: 채원님만을 위한 시간 수정

--- src/main/java/com/study/petory/common/scheduler/UserRestoreScheduler.java
+++ src/main/java/com/study/petory/common/scheduler/UserRestoreScheduler.java
@@ -14,7 +14,7 @@ public class UserRestoreScheduler {
 	private final UserSchedulerService userSchedulerService;
 
 	// 유저 자동 복구 스케줄러 (userStatus = SUSPENDED, deletedAt = 현 시각 기준 30일 된 유저)
-	@Scheduled(cron = "0 0 4 * * *", zone = "Asia/Seoul")  // 매일 새벽 4시: 계정 정지 유저 복구 실행
+	@Scheduled(cron = "0 0 5 * * *", zone = "Asia/Seoul")  // 매일 새벽 5시: 계정 정지 유저 복구 실행
 	public void restoreSuspendedUsers() {
 
 		userSchedulerService.restoreSuspendedUsers();

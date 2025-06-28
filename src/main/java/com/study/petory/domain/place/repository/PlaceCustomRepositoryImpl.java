467c5743a345a33a85b116b732b6fd039c966c74
jhyang22 <jaeho9229@gmail.com>
2025-06-28 16:56:45 +0900

    feat : 주소 검색 조건, 좋아요 기반 인기 랭킹 쿼리

--- src/main/java/com/study/petory/domain/place/repository/PlaceCustomRepositoryImpl.java
+++ src/main/java/com/study/petory/domain/place/repository/PlaceCustomRepositoryImpl.java
@@ -1,7 +1,9 @@
 package com.study.petory.domain.place.repository;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
@@ -32,12 +34,17 @@ public class PlaceCustomRepositoryImpl implements PlaceCustomRepository {
 	private final QUser qUser = QUser.user;
 
 	@Override
-	public Page<PlaceGetAllResponseDto> findAllPlace(String placeName, PlaceType placeType, Pageable pageable) {
+	public Page<PlaceGetAllResponseDto> findAllPlace(String placeName, PlaceType placeType, String address,
+		Pageable pageable) {
 
 		BooleanBuilder booleanBuilder = new BooleanBuilder();
 
 		if (placeName != null && !placeName.isBlank()) {
-			booleanBuilder.and(qPlace.placeName.containsIgnoreCase(placeName));
+			booleanBuilder.or(qPlace.placeName.containsIgnoreCase(placeName));
+		}
+
+		if (address != null && !address.isBlank()) {
+			booleanBuilder.or(qPlace.address.containsIgnoreCase(address));
 		}
 
 		if (placeType != null) {
@@ -101,6 +108,38 @@ public class PlaceCustomRepositoryImpl implements PlaceCustomRepository {
 		}
 
 		return placeList.stream()
+			.map(PlaceGetAllResponseDto::from)
+			.toList();
+	}
+
+	// 장소 타입별로 좋아요 순서로 랭킹 1등만 가져오는데
+	// 장소 타입이 지금 대충 5개 있어요 [카페 좋아요 1등, 숙소 좋아요 1등]
+	// 총 5개의 랭킹이 출력되게끔
+	@Override
+	public List<PlaceGetAllResponseDto> findPlaceRankOrderByLikeCountDescV2() {
+		List<Place> placeList = jpaQueryFactory
+			.selectFrom(qPlace)
+			.where(
+				qPlace.placeStatus.eq((PlaceStatus.ACTIVE)),
+				qPlace.likeCount.isNotNull()
+			)
+			.orderBy(
+				qPlace.placeType.asc(),
+				qPlace.likeCount.desc(),
+				qPlace.ratio.desc().nullsLast(),
+				qPlace.id.asc()
+			)
+			.fetch();
+
+		Map<PlaceType, Place> placeTypePlaceMap = new LinkedHashMap<>();
+		for (Place place : placeList) {
+			PlaceType placeType = place.getPlaceType();
+			if (!placeTypePlaceMap.containsKey(placeType)) {
+				placeTypePlaceMap.put(placeType, place);
+			}
+		}
+
+		return placeTypePlaceMap.values().stream()
 			.map(PlaceGetAllResponseDto::from)
 			.toList();
 	}

746d3cd7aaebd20b1f90c4dfe092962bf78affa2
jhyang22 <jaeho9229@gmail.com>
2025-06-30 02:59:43 +0900

    feat : validation 추가

--- src/main/java/com/study/petory/domain/place/dto/request/PlaceReportRequestDto.java
+++ src/main/java/com/study/petory/domain/place/dto/request/PlaceReportRequestDto.java
@@ -1,5 +1,7 @@
 package com.study.petory.domain.place.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -7,5 +9,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class PlaceReportRequestDto {
 
+	@NotBlank
+	@Size(max = 255, message = "신고 이유는 255자까지만 입력 가능합니다.")
 	private final String content;
 }
--- src/main/java/com/study/petory/domain/place/dto/request/PlaceStatusChangeRequestDto.java
+++ src/main/java/com/study/petory/domain/place/dto/request/PlaceStatusChangeRequestDto.java
@@ -1,5 +1,6 @@
 package com.study.petory.domain.place.dto.request;
 
+import com.study.petory.common.util.ValidEnum;
 import com.study.petory.domain.place.entity.PlaceStatus;
 
 import lombok.Getter;
@@ -9,5 +10,6 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class PlaceStatusChangeRequestDto {
 
+	@ValidEnum(enumClass = PlaceStatus.class, message = "지원하지 않는 상태값입니다.")
 	private final PlaceStatus placeStatus;
 }

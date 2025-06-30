12aad07b353504c75d1110d6bbe095b320abd6dd
deabaind <deabaind@gmail.com>
2025-06-30 23:23:54 +0900

    refactor: 공개 설정 여부 @NotNull
    refactor: 권한에 따라 등록 가능한 이미지 개수 확인 메서드를 클래스로 분리

--- src/main/java/com/study/petory/domain/album/dto/request/AlbumCreateRequestDto.java
+++ src/main/java/com/study/petory/domain/album/dto/request/AlbumCreateRequestDto.java
@@ -2,6 +2,7 @@ package com.study.petory.domain.album.dto.request;
 
 import com.study.petory.domain.album.entity.AlbumVisibility;
 
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,6 +14,6 @@ public class AlbumCreateRequestDto {
 	@Size(max = 255, message = "85글자 이하로 입력해주세요.")
 	private String content;
 
-	// 기본 값: public
+	@NotNull(message = "공개 여부를 입력해주세요.")
 	private AlbumVisibility albumVisibility;
 }

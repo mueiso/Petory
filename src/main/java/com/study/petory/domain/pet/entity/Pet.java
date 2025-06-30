3d57b7315f7826a3364ccf5603c1174240bff298
Jess <jcscene@gmail.com>
2025-06-30 11:02:35 +0900

    feat: Pet 도메인 구현 시작

--- src/main/java/com/study/petory/domain/pet/entity/Pet.java
+++ src/main/java/com/study/petory/domain/pet/entity/Pet.java
@@ -1,12 +1,19 @@
 package com.study.petory.domain.pet.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.study.petory.common.entity.TimeFeatureBasedEntity;
+import com.study.petory.domain.user.entity.User;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -20,15 +27,45 @@ public class Pet extends TimeFeatureBasedEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@Column(nullable = false)
 	private String name;
 
+	@Column
 	private String size;
 
+	@Column
 	private String species;
 
+	@Column
 	private String gender;
 
+	@Column
 	private String birthday;
 
 	private String photo;
+
+	@JsonBackReference
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@Builder
+	public Pet(String name, String size, String species, String gender, String birthday, User user) {
+		this.name = name;
+		this.size =size;
+		this.species = species;
+		this.gender = gender;
+		this.birthday = birthday;
+		this.user = user;
+	}
+
+	public void updatePetInfo(String name, String size, String gender) {
+		this.name = name;
+		this.size = size;
+		this.gender = gender;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
 }

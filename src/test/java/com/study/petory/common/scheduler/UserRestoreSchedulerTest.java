c5872f5b1ca871cfe142cc13f85ba444923bdab2
Jess <jcscene@gmail.com>
2025-07-01 17:27:35 +0900

    test: 주석 설명 추가

--- src/test/java/com/study/petory/common/scheduler/UserRestoreSchedulerTest.java
+++ src/test/java/com/study/petory/common/scheduler/UserRestoreSchedulerTest.java
@@ -22,10 +22,15 @@ class UserRestoreSchedulerTest {
 	@Test
 	void restoreSuspendedUsers_호출시_서비스메서드_실행() {
 
-		// when: 테스트 대상 메서드 직접 호출
+		/* [when]
+		 * 테스트 대상인 restoreSuspendedUsers() 메서드 직접 호출
+		 * 스케줄링 없이 수동으로 실행
+		 */
 		userRestoreScheduler.restoreSuspendedUsers();
 
-		// then: 내부 서비스 메서드가 호출되었는지 검증
+		/* [then]
+		 * 내부적으로 userSchedulerService.restoreSuspendedUsers()가 호출되었는지 검증
+		 */
 		verify(userSchedulerService).restoreSuspendedUsers();
 	}
 }

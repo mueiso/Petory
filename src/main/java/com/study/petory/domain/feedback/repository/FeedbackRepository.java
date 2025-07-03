34879c00e9fe804a08fd67c9ad8717c49819d860
deabaind <deabaind@gmail.com>
2025-07-03 21:20:45 +0900

    refactor: 사용하지 않는 클래스 삭제

--- src/main/java/com/study/petory/domain/feedback/repository/FeedbackRepository.java
+++
@@ -1,8 +0,0 @@
-package com.study.petory.domain.feedback.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.study.petory.domain.feedback.entity.Feedback;
-
-public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
-}

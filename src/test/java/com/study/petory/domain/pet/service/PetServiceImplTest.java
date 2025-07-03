fc795b1dad1ee3d683388d0ebdc9cca3337ccdc4
Jess <jcscene@gmail.com>
2025-07-04 00:06:25 +0900

    test: savePet() 메서드 성공 테스트코드 작성

---
+++ src/test/java/com/study/petory/domain/pet/service/PetServiceImplTest.java
@@ -0,0 +1,58 @@
+package com.study.petory.domain.pet.service;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.study.petory.common.exception.CustomException;
+import com.study.petory.common.exception.enums.ErrorCode;
+import com.study.petory.domain.pet.dto.PetCreateRequestDto;
+import com.study.petory.domain.pet.dto.PetUpdateRequestDto;
+import com.study.petory.domain.pet.entity.Pet;
+import com.study.petory.domain.pet.entity.PetSize;
+import com.study.petory.domain.pet.repository.PetRepository;
+import com.study.petory.domain.user.entity.User;
+import com.study.petory.domain.user.service.UserService;
+
+@ExtendWith(MockitoExtension.class)
+class PetServiceImplTest {
+
+	@Mock
+	private UserService userService;
+
+	@Mock
+	private PetRepository petRepository;
+
+	@InjectMocks
+	private PetServiceImpl petService;
+
+	@Test
+	void savePet_등록_성공() {
+
+		// given
+		Long userId = 1L;
+		PetCreateRequestDto requestDto = new PetCreateRequestDto("쿠키", PetSize.SMALL, "푸들", "남", "2022-01-01");
+
+		User mockUser = mock(User.class);
+		given(userService.findUserById(userId)).willReturn(mockUser);
+
+		ArgumentCaptor<Pet> petCaptor = ArgumentCaptor.forClass(Pet.class);
+
+		// when
+		petService.savePet(userId, requestDto, null);
+
+		// then
+		verify(petRepository).save(petCaptor.capture());
+		verify(mockUser).addPet(any(Pet.class)); // 양방향 연관관계 설정 확인
+		assertThat(petCaptor.getValue().getName()).isEqualTo("쿠키");
+	}
+}

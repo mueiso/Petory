5b44db308ed29af73a1c7fc620460d27e1eb6db5
chaewon9999 <w0729@naver.com>
2025-07-04 16:51:48 +0900

    fix: 채팅 html 수정

---
+++ src/test/java/com/study/petory/domain/chat/repository/ChatRepositoryTest.java
@@ -0,0 +1,44 @@
+// package com.study.petory.domain.chat.repository;
+//
+// import static com.mysema.commons.lang.Assert.*;
+// import static org.junit.jupiter.api.Assertions.*;
+//
+// import java.util.List;
+//
+// import org.bson.types.ObjectId;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+// import org.springframework.data.domain.PageRequest;
+// import org.springframework.test.util.ReflectionTestUtils;
+//
+// import com.study.petory.domain.chat.entity.ChatRoom;
+//
+// @DataMongoTest
+// class ChatRepositoryTest {
+//
+// 	@Autowired
+// 	private ChatRepository chatRepository;
+//
+// 	@Test
+// 	void 유저_아이디로_채팅방_찾기에_성공한다() {
+// 		//given
+// 		PageRequest pageable = PageRequest.of(0, 10);
+//
+// 		ChatRoom chatRoom = ChatRoom.builder()
+// 			.tradeBoardId(1L)
+// 			.sellerId(1L)
+// 			.customerId(2L)
+// 			.build();
+// 		ReflectionTestUtils.setField(chatRoom, "id", new ObjectId());
+//
+// 		//when
+// 		List<ChatRoom> chatRooms = chatRepository.findChatRoomsByUserId(1L, pageable);
+// 		chatRepository.save(chatRoom);
+//
+// 		//then
+// 		assertEquals(1, chatRooms.size());
+// 		assertEquals(1L, chatRooms.get(0).getTradeBoardId());
+// 		assertEquals(2L, chatRooms.get(0).getCustomerId());
+// 	}
+// }

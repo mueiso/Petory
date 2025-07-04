abd046cdb844b02bf52d2f55845e38d68506798d
Jess <jcscene@gmail.com>
2025-07-04 11:52:19 +0900

    test: 주석 설명 추가

--- src/test/java/com/study/petory/domain/pet/dto/PetResponseDtoTest.java
+++ src/test/java/com/study/petory/domain/pet/dto/PetResponseDtoTest.java
@@ -17,33 +17,51 @@ class PetResponseDtoTest {
 	@Test
 	void of_정상적으로_DTO_생성됨() {
 
-		// given
-		User user = new User(1L);  // 최소한의 유저 객체 (ID만 설정)
+		/* [given]
+		 * 최소한의 유저 객체 (ID만 설정)
+		 */
+		User user = new User(1L);
 
+		// 테스트용 Pet 객체 생성
 		Pet pet = Pet.builder()
-			.name("코코")
+			.name("쿠키")
 			.size(PetSize.SMALL)
 			.species("푸들")
-			.gender("암컷")
+			.gender("수컷")
 			.birthday("2022-01-01")
 			.user(user)
 			.build();
 
-		// createdAt 설정 (TimeFeatureBasedEntity에서 normally 자동 처리되지만, 수동으로 설정 필요)
+		/*
+		 * createdAt 은 TimeFeatureBasedEntity 상속으로 자동 생성되지만 테스트에서는 수동으로 설정 필요
+		 * ReflectionTestUtils 이용해 강제로 필드값 주입
+		 * 반려동물 이미지 url 리스트 설정
+		 */
 		LocalDateTime now = LocalDateTime.of(2025, 1, 1, 10, 0);
 		ReflectionTestUtils.setField(pet, "createdAt", now);
-
 		List<String> imageUrls = List.of("https://example.com/image1.jpg", "https://example.com/image2.jpg");
 
-		// when
+		/* [when]
+		 * DTO 변환 메서드 호출
+		 */
 		PetResponseDto dto = PetResponseDto.of(pet, imageUrls);
 
-		// then
-		assertThat(dto.getId()).isNull();  // id는 null → persist 안했기 때문
-		assertThat(dto.getName()).isEqualTo("코코");
+		/* [then]
+		 * (변환된 DTO 값 검증)
+		 * id 는 persist 안했기 때문에 DB 저장 전이라 null
+		 * 이름 확인
+		 * 사이즈 확인
+		 * 종 확인
+		 * 성별 확인
+		 * 생일 확인
+		 * 이미지 url 리스트 확인
+		 * 생성일 확인
+		 */
+		assertThat(dto.getId()).isNull();
+		assertThat(dto.getName()).isEqualTo("쿠키");
 		assertThat(dto.getSize()).isEqualTo(PetSize.SMALL);
 		assertThat(dto.getSpecies()).isEqualTo("푸들");
-		assertThat(dto.getGender()).isEqualTo("암컷");
+		assertThat(dto.getGender()).isEqualTo("수컷");
 		assertThat(dto.getBirthday()).isEqualTo("2022-01-01");
 		assertThat(dto.getImageUrls()).containsExactly("https://example.com/image1.jpg", "https://example.com/image2.jpg");
 		assertThat(dto.getCreatedAt()).isEqualTo(now);

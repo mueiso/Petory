431c6a970e3a00b7a09ae559c3b4154395794014
Jess <jcscene@gmail.com>
2025-07-05 19:23:40 +0900

    fix: 유저 조회 시 userStatus 함께 조회할 쿼리 로직 분리 형태로 추가

--- src/main/java/com/study/petory/common/exception/enums/ErrorCode.java
+++ src/main/java/com/study/petory/common/exception/enums/ErrorCode.java
@@ -36,6 +36,7 @@ public enum ErrorCode implements BaseCode {
 	LOGIN_UNAVAILABLE(HttpStatus.FORBIDDEN, "로그인 불가합니다. 계정이 정지되었거나, 탈퇴한 유저입니다."),
 	ALREADY_SUSPENDED(HttpStatus.BAD_REQUEST, "이미 정지된 계정입니다."),
 	USER_NOT_DEACTIVATED(HttpStatus.BAD_REQUEST, "비활성화된 유저가 아닙니다."),
+	USER_NOT_SUSPENDED(HttpStatus.BAD_REQUEST, "계정 정지된 유저가 아닙니다."),
 	USER_ALREADY_DELETED(HttpStatus.BAD_REQUEST, "이미 탈퇴한 계정입니다."),
 	USER_NOT_EXISTING(HttpStatus.BAD_REQUEST, "휴면 계정이거나, 탈퇴한 유저입니다."),
 
--- src/main/java/com/study/petory/domain/user/repository/UserQueryRepositoryImpl.java
+++ src/main/java/com/study/petory/domain/user/repository/UserQueryRepositoryImpl.java
@@ -5,6 +5,7 @@ import static com.study.petory.domain.user.entity.QUserRole.*;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.study.petory.domain.user.entity.User;
+import com.study.petory.domain.user.entity.UserStatus;
 
 import lombok.RequiredArgsConstructor;
 
@@ -35,6 +36,20 @@ public class UserQueryRepositoryImpl implements UserQueryRepository {
 				.selectFrom(user)
 				.leftJoin(user.userRole, userRole).fetchJoin()
 				.where(user.id.eq(id))
+				.fetchOne()
+		);
+	}
+
+	@Override
+	public Optional<User> findByIdWithUserRoleAndUserStatus(Long id, UserStatus status) {
+		return Optional.ofNullable(
+			queryFactory
+				.selectFrom(user)
+				.leftJoin(user.userRole, userRole).fetchJoin()
+				.where(
+					user.id.eq(id),
+					user.userStatus.eq(status)
+				)
 				.fetchOne()
 		);
 	}

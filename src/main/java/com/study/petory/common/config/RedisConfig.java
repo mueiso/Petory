5cdf3a4664d2f6fb5902a009076642427a6a31ce
deabaind <deabaind@gmail.com>
2025-07-07 11:04:32 +0900

    refactor: loginRefreshToken Template value 타입 수정

--- src/main/java/com/study/petory/common/config/RedisConfig.java
+++ src/main/java/com/study/petory/common/config/RedisConfig.java
@@ -16,9 +16,9 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import io.github.bucket4j.distributed.ExpirationAfterWriteStrategy;
 import io.github.bucket4j.distributed.proxy.ProxyManager;
@@ -84,7 +84,7 @@ public class RedisConfig {
 	@Bean(name = "loginRefreshToken")
 	public RedisTemplate<String, String> loginRefreshToken() {
 		RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-		redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<Object>(Object.class));
+		redisTemplate.setValueSerializer(new StringRedisSerializer());
 		redisTemplate.setConnectionFactory(redisConnectionFactory());
 		return redisTemplate;
 	}

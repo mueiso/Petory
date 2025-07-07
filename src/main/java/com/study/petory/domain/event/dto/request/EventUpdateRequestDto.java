27a7ff38f7435d09eaf001aefede5eaceb430510
jhyang22 <jaeho9229@gmail.com>
2025-07-07 23:12:39 +0900

    refactor : validation 추가

--- src/main/java/com/study/petory/domain/event/dto/request/EventUpdateRequestDto.java
+++ src/main/java/com/study/petory/domain/event/dto/request/EventUpdateRequestDto.java
@@ -6,6 +6,7 @@ import com.study.petory.common.util.ValidEnum;
 import com.study.petory.domain.event.entity.EventColor;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -13,15 +14,19 @@ import lombok.Getter;
 @AllArgsConstructor
 public class EventUpdateRequestDto {
 
+	@Size(max = 100, message = "최대 100자 입력할 수 있습니다.")
 	@NotBlank(message = "제목은 필수 항목입니다.")
 	private String title;
 
+	@Size(max = 30, message = "최대 30자 입력할 수 있습니다.")
 	@NotBlank(message = "시작 일은 필수 항목입니다.")
 	private String startDate;
 
+	@Size(max = 30, message = "최대 30자 입력할 수 있습니다.")
 	@NotBlank(message = "종료 일은 필수 항목입니다.")
 	private String endDate;
 
+	@Size(max = 20, message = "최대 20자 입력할 수 있습니다.")
 	@NotBlank(message = "타임 존은 필수 항목입니다.")
 	private String timeZone;
 
@@ -29,6 +34,7 @@ public class EventUpdateRequestDto {
 
 	private List<String> recurrence;
 
+	@Size(max = 300, message = "최대 300자 입력할 수 있습니다.")
 	private String description;
 
 	@ValidEnum(enumClass = EventColor.class, ignoreCase = true, message = "지원하지 않는 색상입니다.")

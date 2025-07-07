2dda65dd1d7d76f96497f222656712692328710e
jhyang22 <jaeho9229@gmail.com>
2025-07-07 20:48:40 +0900

    docs : 안쓰는 파일 삭제

--- src/test/java/com/study/petory/domain/notification/controller/NotificationControllerTest.java
+++
@@ -1,7 +0,0 @@
-package com.study.petory.domain.notification.controller;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class NotificationControllerTest {
-
-}
--- src/test/java/com/study/petory/domain/tradeboard/controller/TradeBoardControllerTest.java
+++
@@ -1,7 +0,0 @@
-package com.study.petory.domain.tradeboard.controller;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class TradeBoardControllerTest {
-
-}
